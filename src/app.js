d04341fcf71a7b07d8a2cbfc341fd17cd867eacc
Mabel Lee <yanhwa.lee@thoughtworks.com>
2020-02-14 15:15:41 +0800

    [mabel] Edit cookie

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
 const corsOptions = {
-  origin: "*",
+  origin: ["http://localhost:3000", "http://localhost:3001"],
   credentials: true,
 };
 
